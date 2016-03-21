3c43e58f33bd70843648926935f4bcd0f7c1406b
Edson Miguel Dewes <edson_dewes@hotmail.com>
2016-03-21 16:43:25 -0300

    Dockerfile inicial

---
+++ entrypoint/config-roles.sql
@@ -0,0 +1,7 @@
+--create role ecidade with superuser login password 'ecidade';
+create role dbseller with login password 'dbseller';
+create role plugin with login password 'plugin';
+create role dbportal with login password 'dbportal';
+
+--CREATE DATABASE "e-cidade";
+--GRANT ALL PRIVILEGES ON DATABASE "e-cidade" to ecidade;
